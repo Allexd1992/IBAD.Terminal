73e01af9f95a46179defad38211d8b858bf0835e
allex <allex@DESKTOP-N68QL0P>
2020-04-13 18:46:09 +0300

    TODO01 add LentaDetect  LogWriter and Modbus Server and client

---
+++ IBAD.Terminal.Library/LentaDetect.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBAD.Terminal.Library
+{
+    public class LentaDetect
+    {
+        public string[] TapeName { get; set; }
+        public double[] TapeStart { get; set; }
+        public double[] TapeEnd { get; set; }
+
+        double[] getInterval(double[] Start, double[] End, double DeltaTape, double Delta)
+        {
+            double[] Out = new double[Start.Length+1];
+
+            for (int i = 0; i < Out.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Out[0] = DeltaTape + Delta;
+                }
+                else
+                {
+                    double Lenght = Math.Abs(End[i - 1] - Start[i - 1]);
+                    Out[i] = Out[i - 1] + Lenght;
+                }
+            }
+
+            return Out;
+        }
+        int TapeDetect(double Lenght, double[] setPoint)
+            {
+            int Out=606;
+            for (int i = 0; i < setPoint.Length; i++)
+            {
+                if  (Lenght<setPoint[i] && Lenght > setPoint[i-1] && i!=0)
+                {
+                    Out= i-1;
+                }
+                else
+                {
+                    if (Lenght < setPoint[0]) Out = -1;
+                    if (Lenght > setPoint[setPoint.Length-1]) Out = setPoint.Length;
+                }
+
+            }
+            return Out;
+            }
+        double positionResolve(double[] Start, double[] End, double Lenght, int setPoin, double[] interval)
+        {
+
+
+            if (setPoin != -1 && setPoin != Start.Length+1 && setPoin != 606 && Start[setPoin] != End[setPoin])
+            {
+                double sign = 1.0;
+                if (Start[setPoin] > End[setPoin])
+                {
+                    sign = -1;
+                }
+
+                return (Lenght - interval[setPoin]) * sign + Start[setPoin];
+            }
+            if (setPoin == -1) return Lenght;
+            if (setPoin == Start.Length + 1) return Lenght - interval[setPoin];
+            else return 0;
+        }
+        string getNameTape(string[] Name, double[] Start, double[] End, int setPoint)
+        {
+            if (setPoint != -1 && setPoint != 11 && setPoint != 606 && Start[setPoint] != End[setPoint])
+            {
+                return "#" + Name[setPoint] + "_" + string.Format("{0:0.00}", Start[setPoint]) + "-" + string.Format("{0:0.00}", End[setPoint]);
+            }
+            if (setPoint == -1) return "TransportStart";
+            if (setPoint == Name.Length+1) return "TransportEnd";
+            return "Error";
+        }
+    }
+}
---
+++ IBAD.Terninal.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IBAD.Terninal.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IBAD.Terninal.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64d6f450-cad1-4626-b296-f0e3a7dae4a3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
